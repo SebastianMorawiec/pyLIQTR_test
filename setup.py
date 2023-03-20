fa3a0e410ff1cf34d6d060118268a319ac793604
Sebastian Morawiec <Sebastian.Morawiec@zapatacomputing.com>
2023-03-20 10:42:31 +0100

    reduce deps to cirq-core

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     package_dir={"": "src"},
     python_requires=">=3.8",
     install_requires=[
-        "cirq",
+        "cirq-core",
         "numpy",
         "pandas",
         "scipy",
